bf7f8079cc1eef225aa18df62df79558c192607c
Misho Ghibra <mishiko8gb@gmail.com>
2024-04-01 21:16:31 +0400

    adding a feedback effects for android and IOS while deleting items on press

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,14 @@ import { View, StyleSheet, Text, Pressable } from "react-native";
 
 const GoalItem = ({ goalItem, onDeleteItem }) => {
   return (
-    <Pressable onPress={() => onDeleteItem(goalItem.item.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        style={({ pressed }) => pressed && styles.pressedItem}
+        onPress={() => onDeleteItem(goalItem.item.id)}
+      >
         <Text style={styles.goalText}>{goalItem.item.goal}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -15,11 +18,16 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
+
     borderRadius: 8,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
+  },
+
+  pressedItem: {
+    opacity: 0.5,
   },
 });
